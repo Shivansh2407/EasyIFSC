f0d9b3a6474a021e9636b3f4adb27b63a57ef94f
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-07-01 18:19:46 +0530

    Views File Updated

--- easyifscapi/views.py
+++ easyifscapi/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Bank, Branch
+
+from .serializers import BranchSerializer
+
+class DetailView(View):
+    def get(self, request, ifsc):
+        branch = Branch.objects.filter(ifsc__iexact=ifsc).first()
+        serializer = BranchSerializer(branch)
+        return JsonResponse(serializer.data, safe=False)
+
+
+class ListView(View):
+    def get(self, request, city, bank):
+        branch_group = Branch.objects.filter(
+            city__iexact=city, bank__name__icontains=bank
+            )
+        serializer = BranchSerializer(branch_group, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
