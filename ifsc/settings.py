8fcdfc2fe081ca978b9c29b2f27497cf444e37bb
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-07-02 17:53:55 +0530

    Static Root Issue Fixed

--- ifsc/settings.py
+++ ifsc/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
